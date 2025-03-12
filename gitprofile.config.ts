119d86c19d61870c4f58943190677c13998c52ed
Alekhya <53375296+Alekhya03@users.noreply.github.com>
2025-03-12 14:59:36 -0500

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -1,6 +1,6 @@
 // gitprofile.config.ts
 
-const CONFIG = {
+const config = {
   github: {
     username: 'Alekhya03', // Your GitHub org/user name. (This is the only required config)
   },
@@ -31,113 +31,159 @@ const CONFIG = {
     imageURL: '',
   },
   social: {
-    //linkedin: 'ariful-alam',
-    //x: 'arif_szn',
-    //mastodon: 'arifszn@mastodon.social',
-    researchGate: '',
-    facebook: '',
-    instagram: '',
-    reddit: '',
-    threads: '',
-    youtube: '', // example: 'pewdiepie'
-    udemy: '',
-    dribbble: '',
-    behance: '',
-   // medium: 'arifszn',
-    //dev: 'arifszn',
-    stackoverflow: '', // example: '1/jeff-atwood'
-    skype: '',
-    telegram: '',
-    //website: 'https://www.arifszn.com',
-    phone: '',
-    //email: 'arifulalamszn@gmail.com',
+    linkedin: 'gvgowtham',
+    // twitter: 'arif_szn',
+    // mastodon: 'arifszn@mastodon.social',
+    // facebook: '',
+    instagram: 'gowtham__g',
+    // youtube: '', // example: 'pewdiepie'
+    // dribbble: '',
+    // behance: '',
+    // medium: 'arifszn',
+    // dev: 'arifszn',
+    // stackoverflow: '', // example: '1/jeff-atwood'
+    // skype: '',
+    // telegram: '',
+    // website: 'https://gowthamgottimukkala.github.io/',
+    // phone: '3527099179',
+    email: 'v.gottimukkala@ufl.edu',
+    avatarUrl: '/Image.jpg',
   },
   resume: {
     fileUrl:
       'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
+    'C/C++',
+    'Python',
+    'Java',
+    'JavaScript (ES6)',
+    'SQL',
+    'React',
+    'Redux',
     'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
+    'Django',
+    'Flask',
+    'SpringBoot',
+    'Socket.io',
     'CSS',
-    'Antd',
-    'Tailwind',
+    'HTML5',
+    'JSON',
+    'REST',
+    'PostgreSQL',
+    'MySQL',
+    'Mongo',
+    'IoT',
+    'AWS',
+    'Kafka',
+    'Docker',
+    'NGINX',
+    'UNIX/Linux',
+    'Git CLI',
+    'GitHub',
+    'BitBucket',
+    'Jenkins',
+    'Postman',
+    'Figma (UI)',
+    'JIRA'
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
+      company: 'UF Health, University of Florida',
+      position: 'Graduate Student Assitant',
+      from: 'March 2023',
+      to: 'May 2024',
+      // companyLink: 'https://example.com',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Lowes India',
+      position: 'Associate Software Engineer',
+      from: 'June 2021',
+      to: 'July 2022',
+      // companyLink: 'https://example.com',
+    },
+    {
+      company: 'Ciphense AI Research Lab',
+      position: 'Software Engineering Intern',
+      from: 'May 2020',
+      to: 'October 2020',
+      // companyLink: 'https://example.com',
+    },
+    {
+      company: 'Oorwin Labs',
+      position: 'Data Science Intern',
+      from: 'May 2019',
+      to: 'June 2019',
+      // companyLink: 'https://example.com',
     },
   ],
-  certifications: [
+  /* certifications: [
     {
       name: 'Lorem ipsum',
       body: 'Lorem ipsum dolor sit amet',
       year: 'March 2022',
-      link: 'https://example.com',
+      link: 'https://example.com'
+    },
+  ], */
+  education: [
+    {
+      institution: 'University of Florida',
+      degree: 'Master of Science, Computer Science',
+      from: 'August 2022',
+      to: 'May 2024',
+    },
+    {
+      institution: 'IIIT, Design and Manufacturing, Kancheepuram',
+      degree: 'Bachelor of Technology, Computer Engineering',
+      from: '2017',
+      to: '2021',
     },
   ],
-  educations: [
-    {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
-    },
-    {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
-    },
+
+  // To hide the `My Projects` section, keep it empty.
+  externalProjects: [
+    // {
+    //   title: 'Project Name',
+    //   description:
+    //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+    //   imageUrl: 'https://via.placeholder.com/250x250',
+    //   link: 'https://example.com',
+    // },
+    // {
+    //   title: 'Project Name',
+    //   description:
+    //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+    //   imageUrl: 'https://via.placeholder.com/250x250',
+    //   link: 'https://example.com',
+    // },
   ],
-  
-  // Display articles from your medium or dev account. (Optional)
+  // Display blog posts from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many articles to display. Max is 10.
+    // source: 'dev', // medium | dev
+    // username: 'arifszn', // to hide blog section, keep it empty
+    // limit: 2, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    // id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
-    id: '',
-    snippetVersion: 6,
+    // id: '',
+    // snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'lofi',
+    defaultTheme: 'autumn',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
-    disableSwitch: false,
+    disableSwitch: true,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
     respectPrefersColorScheme: false,
 
-    // Display the ring in Profile picture
-    displayAvatarRing: true,
+    // Hide the ring in Profile picture
+    hideAvatarRing: false,
 
     // Available themes. To remove any theme, exclude from here.
     themes: [
@@ -170,13 +216,10 @@ const CONFIG = {
       'night',
       'coffee',
       'winter',
-      'dim',
-      'nord',
-      'sunset',
       'procyon',
     ],
 
-    // Custom theme, applied to `procyon` theme
+    // Custom theme
     customTheme: {
       primary: '#fc055b',
       secondary: '#219aaf',
@@ -189,13 +232,7 @@ const CONFIG = {
   },
 
   // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> and ❤️`,
-
-  enablePWA: true,
+  footer: `Made with ❤️`,
 };
 
-export default CONFIG;
+export default config;
